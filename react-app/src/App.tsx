79d6d455bfd295683083fae168f76643d4bf7db3
Sandrow <sandrow.townsend@hotmail.com>
2023-10-16 09:09:03 -0400

    React Practice

---
+++ react-app/src/App.tsx
@@ -0,0 +1,25 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        Hello <span>world</span>
+      </Alert>
+      <Button onClick={() => console.log("Clicked")}>new button3</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No Items Found</p>}
+      <ul className="list-group">
+        {items.map((items, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={items}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(items);
+            }}
+          >
+            {items}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
